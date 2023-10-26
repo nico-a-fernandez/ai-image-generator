da5ffff7ced43b36b1db8a126da9a6af57ebe544
nico-a-fernandez <117313555+nico-a-fernandez@users.noreply.github.com>
2023-10-26 16:20:40 -0300

    Connected to API
    connected to OpenAI, functionally working, some styling details to complete.

--- README.md
+++ README.md
@@ -68,3 +68,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+secret key: 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
--- src/components/ImageGenerator/ImageGenerator.jsx
+++ src/components/ImageGenerator/ImageGenerator.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./ImageGenerator.css";
 import default_image from "../Assets/default_image.svg";
 export const ImageGenerator = () => {
+	const [image_url, setImage_url] = useState("/");
+	let inputRef = useRef(null);
+
+	const imageGenerator = async () => {
+		if (inputRef.current.value === "") {
+			return 0;
+		}
+		const response = await fetch(
+			"https://api.openai.com/v1/images/generations",
+			{
+				method: "POST",
+				headers: {
+					"content-Type": "application/json",
+					authorization:
+						"Bearer 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",
+					"User-Agent": "Chrome",
+				},
+				body: JSON.stringify({
+					prompt: `${inputRef.current.value}`,
+					n: 1,
+					size: "512x512",
+				}),
+			}
+		);
+		let data = await response.json();
+		let data_array = data.data;
+		setImage_url(data_array[0].url);
+	};
+
 	return (
 		<div className="ai-image-generator">
 			<div className="header">
@@ -9,16 +38,27 @@ export const ImageGenerator = () => {
 			</div>
 			<div className="img-loading">
 				<div className="image">
-					<img src={default_image} alt="default stock image" />
+					<img
+						src={image_url === "/" ? default_image : image_url}
+						alt="default stock image"
+					/>
 				</div>
 			</div>
 			<div className="search-box">
 				<input
 					type="text"
+					ref={inputRef}
 					className="search-input"
 					placeholder="Describe what you want to see"
 				/>
-				<div className="generate-btn">Generate</div>
+				<div
+					className="generate-btn"
+					onClick={() => {
+						imageGenerator();
+					}}
+				>
+					Generate
+				</div>
 			</div>
 		</div>
 	);

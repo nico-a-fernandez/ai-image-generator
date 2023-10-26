83e954fa7253f90d81130abfbca7b6293e2cd298
nico-a-fernandez <117313555+nico-a-fernandez@users.noreply.github.com>
2023-10-26 15:36:54 -0300

    CSS optimization
    Changing background colors, font colors, text and image sizes, adding input and buttons

--- src/components/ImageGenerator/ImageGenerator.jsx
+++ src/components/ImageGenerator/ImageGenerator.jsx
@@ -12,6 +12,14 @@ export const ImageGenerator = () => {
 					<img src={default_image} alt="default stock image" />
 				</div>
 			</div>
+			<div className="search-box">
+				<input
+					type="text"
+					className="search-input"
+					placeholder="Describe what you want to see"
+				/>
+				<div className="generate-btn">Generate</div>
+			</div>
 		</div>
 	);
 };
